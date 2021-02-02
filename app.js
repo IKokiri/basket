fc0e5aeeadd8020db0f9d12f63b9feb7faa1cf0b
Luiz Mendes <developer.luizmendes@gmail.com>
2021-02-02 00:06:46 -0300

    rotas

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ const express = require('express')
 const app = express()
 const routes = require('./src/routes');
 require('dotenv').config()
+app.use(express.json())
 
 const port = process.env.PORT
 
 app.use('/',routes);
 
-
-app.listen(port, () => {
-  console.log(`${port}`)
-})
+app.listen(port, () => {})
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,27 @@
 const routes = require('express').Router();
 
 routes.get('/', (req, res) => {
-    res.send(`
-        <html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1>Rota padrão</h1>
-            </body>
-        </html>
-    `);
+    res.send(`Busca todos`);
+});
+
+routes.get('/:id', (req, res) => {
+    res.send(`busca ${req.params.id}`);
+});
+
+routes.post('/', (req, res) => {
+    console.log(req.body)
+    res.send("inserido")
+});
+
+routes.put('/:id', (req, res) => {
+    console.log(req.params)
+    console.log(req.body)
+    res.send(`alterado`);
+});
+
+routes.delete('/:id', (req, res) => {
+    console.log(req.params)
+    res.send(`removido`);
 });
 
 module.exports = routes;
